1d598d12feaace0a85640d36abf066c95786329c
developerTamanna <astamanna57@gmail.com>
2025-09-03 19:39:55 +0600

    array index.js

---
+++ array.js
@@ -0,0 +1,2 @@
+var public = [10, 20, 30, 20,30,40,30,20,23,]
+console.log(public.indexOf(20));
---
+++ practice2.js
@@ -0,0 +1,9 @@
+// let hero = typeof 'Superman'
+// console.log(hero)
+
+// var batKhaiso = false;
+// console.log( typeof batKhaiso)
+
+var slot = 10;
+slot = 20;
+console.log(slot);
